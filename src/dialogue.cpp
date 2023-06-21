035de905a3406a5dd9bc3b60d1ea72240c0b5cb2
Jelly <sarthakrai3406@gmail.com>
2023-06-21 08:07:07 +0530

    std::round instead of static cast int

--- src/dialogue.cpp
+++ src/dialogue.cpp
@@ -1,6 +1,7 @@
 #include "dialogue.h"
 
 #include "nlohmann/json.hpp"
+#include <cmath>
 #include <cstdio>
 #include <exception>
 #include <fstream>
@@ -99,39 +100,51 @@ void Dialogue::SetContainerPosition(ContainerPosition position) {
 
 void Dialogue::CalculateContainerSize() {
     // Set containerSX to be 67.5% of the screen width
-    container.contBox.width = static_cast<int>(GetScreenWidth() * 0.675);
+    container.contBox.width = static_cast<float>(GetScreenWidth() * 0.675);
     // Set containerSY to be 20% of the screen height
-    container.contBox.height = static_cast<int>(GetScreenHeight() * 0.2);
+    container.contBox.height = static_cast<float>(GetScreenHeight() * 0.2);
+
+    container.contBox.width = std::round(container.contBox.width);
+    container.contBox.height = std::round(container.contBox.height);
 }
 
 void Dialogue::SetImage(const Texture2D& imageTexture) {
     container.charImage = imageTexture;
 
     // Set imageSX to be 18.52% of container
-    container.charImage.width = static_cast<int>(container.contBox.width * 0.1852);
+    container.charImage.width = static_cast<float>(container.contBox.width * 0.1852);
     // Set imageSY to be 83.3% of container
-    container.charImage.height = static_cast<int>(container.contBox.height * 0.8333);
+    container.charImage.height = static_cast<float>(container.contBox.height * 0.8333);
 
     // Set imagePX to be 1.85% of container
-    container.imagePosition.x = static_cast<int>(GetScreenWidth() - ((container.contBox.width * 0.0185) + container.charImage.width));
+    container.imagePosition.x = static_cast<float>((container.contBox.width * 0.0185) + container.charImage.width);
     // Set imagePY to be 8.3% of container
-    container.imagePosition.x = static_cast<int>(GetScreenHeight() - ((container.contBox.width * 0.083) + container.charImage.height));
+    container.imagePosition.y = static_cast<float>(GetScreenHeight() - ((container.contBox.height * 0.083) + container.charImage.height));
+    
+    container.charImage.width = std::round(container.charImage.width);
+    container.charImage.height = std::round(container.charImage.height);
+    container.imagePosition.x = std::round(container.imagePosition.x);
+    container.imagePosition.y = std::round(container.imagePosition.y);
+
+    std::cerr << container.imagePosition.x << ", " << container.imagePosition.y << "\n";
 }
 
 void Dialogue::SetTextbox() {
     // Set textboxSX to be 76% of container
-    container.textBox.width = static_cast<int>(container.contBox.width * 0.76);
+    container.textBox.width = static_cast<float>(container.contBox.width * 0.76);
     // Set textboxSY to be 83.3% of container
-    container.textBox.height = static_cast<int>(container.contBox.height * 0.8333);
+    container.textBox.height = static_cast<float>(container.contBox.height * 0.8333);
 
 
     // Set textboxPX to be 24% of container
-    container.textBox.x = static_cast<int>(GetScreenWidth() - ((container.contBox.width * 0.24) + container.textBox.width));
+    container.textBox.x = static_cast<float>(GetScreenWidth() - ((container.contBox.width * 0.24) + container.textBox.width));
     // Set textboxPY to be 8.3% of container
-    container.textBox.y = static_cast<int>(GetScreenHeight() - ((container.contBox.height * 0.083) + container.textBox.height));
+    container.textBox.y = static_cast<float>(GetScreenHeight() - ((container.contBox.height * 0.083) + container.textBox.height));
 
-    std::cerr << container.textBox.x << ", " << container.textBox.y << "\n";
-    std::cerr << container.textBox.width << ", " << container.textBox.height << "\n";
+    container.textBox.width = std::round(container.textBox.width);
+    container.textBox.height = std::round(container.textBox.height);
+    container.textBox.x = std::round(container.textBox.x);
+    container.textBox.y = std::round(container.textBox.y);
 }
 
 void Dialogue::DrawDialogueText(const int fontSize, const int fontSpacing) {
@@ -180,4 +193,12 @@ void Dialogue::CreateDialogueBox(Color outline, Color textbox, Color imageTint) 
         textbox
     );
 
+    // TESTING PURPOSES ONLY -- Image
+    DrawRectangle(
+        container.imagePosition.x,
+        container.imagePosition.y,
+        container.charImage.width,
+        container.charImage.height,
+        imageTint
+    );
 }
